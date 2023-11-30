5adfa770aea5833e6dbc379f66068ec5d9d38959
astralcorp <152506977+astralcorp@users.noreply.github.com>
2023-11-30 16:54:46 +0300

    Add files via upload

---
+++ publishVelocity.py
@@ -0,0 +1,87 @@
+#! /usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import TwistStamped,PoseStamped
+from mavros_msgs.msg import State
+from mavros_msgs.srv import CommandBool, CommandBoolRequest, SetMode, SetModeRequest
+
+current_state = State()
+
+def state_cb(msg):
+    global current_state
+    current_state = msg
+
+
+if __name__ == "__main__":
+    rospy.init_node("offb_node_py")
+
+    state_sub = rospy.Subscriber("mavros/state", State, callback = state_cb)
+    local_pos_pub = rospy.Publisher("mavros/setpoint_position/local", PoseStamped, queue_size=10)
+    vel_pub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel',TwistStamped, queue_size=0) 
+
+    rospy.wait_for_service("/mavros/cmd/arming")
+    arming_client = rospy.ServiceProxy("mavros/cmd/arming", CommandBool)
+
+    rospy.wait_for_service("/mavros/set_mode")
+    set_mode_client = rospy.ServiceProxy("mavros/set_mode", SetMode)
+
+
+    # Setpoint publishing MUST be faster than 2Hz
+    rate = rospy.Rate(20)
+
+    # Wait for Flight Controller connection
+    while(not rospy.is_shutdown() and not current_state.connected):
+        rate.sleep()
+
+    pose = PoseStamped()
+
+    pose.pose.position.x = 0
+    pose.pose.position.y = 0
+    pose.pose.position.z = 2
+
+    # Send a few setpoints before starting
+    for i in range(100):
+        if(rospy.is_shutdown()):
+            break
+
+        local_pos_pub.publish(pose)
+        rate.sleep()
+
+    offb_set_mode = SetModeRequest()
+    offb_set_mode.custom_mode = 'OFFBOARD'
+
+    arm_cmd = CommandBoolRequest()
+    arm_cmd.value = True
+
+    last_req = rospy.Time.now()
+
+    while(not rospy.is_shutdown()):
+        if(current_state.mode != "OFFBOARD" and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+            if(set_mode_client.call(offb_set_mode).mode_sent == True):
+                rospy.loginfo("OFFBOARD enabled")
+
+            last_req = rospy.Time.now()
+        else:
+            if(not current_state.armed and (rospy.Time.now() - last_req) > rospy.Duration(5.0)):
+                if(arming_client.call(arm_cmd).success == True):
+                    rospy.loginfo("Vehicle armed")
+
+                last_req = rospy.Time.now()
+
+        
+        for i in range (20):
+            local_pos_pub.publish(pose)# first take off to 2m
+            rate.sleep()
+
+
+
+        velocity_msg = TwistStamped()
+        velocity_msg.header.stamp = rospy.Time.now()
+
+        velocity_msg.twist.linear.x = 0.1 # m/s positive for going right negative for going left
+        velocity_msg.twist.linear.y = 0 # positive for going forward negative for going backward
+        velocity_msg.twist.linear.z = 0 # positive for going up negative for going down
+          
+        vel_pub.publish(velocity_msg)
+
+        rate.sleep()
