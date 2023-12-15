8ccc76256f56670f3a7badbd40360c4551b0ea19
astralcorp <152506977+astralcorp@users.noreply.github.com>
2023-12-15 13:41:50 +0300

    Delete env.sh

--- env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/catkin_ws && catkin build
-sudo env PATH=$PATH:/home/$USER/.nvm/versions/node/v12.14.1/bin /home/$USER/.nvm/versions/node/v12.14.1/lib/node_modules/pm2/bin/pm2 startup systemd -u $USER --hp /home/$USER
-
